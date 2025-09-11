14983ff091eb0962b35ea298628232897ead09ca
mohit <ahirwalmohit3216@gmail.com>
2025-09-11 06:00:16 +0530

    update bycrmpt file

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
